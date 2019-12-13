da1f92d4b4c5aaf140d7d953dcdfdef17d019dc6
Rafael Zasas <zasas.rafi@gmail.com>
2019-12-12 21:31:07 -0800

    testing buttons

--- js/FinalAssignment.js
+++ js/FinalAssignment.js
@@ -1,7 +1,29 @@
 function Register() {
 
+   // obtain information
+    var firstName = document.getElementById("FirstName").value;
+    var lastName = document.getElementById("LastName").value;
+    var email = document.getElementById("Email").value;
+    var password = document.getElementById("Password").value;
+
+    if (password.isNumeric()){
+        alert("works");
+        console.log("password check works");
+    } else alert("Enter numbers only");
+
+
+
 }
 
 function Login() {
+
+    // obtain information
+    var email = document.getElementById("Email").value;
+    var password = document.getElementById("Password").value;
+
+    if (password.isNumeric()){
+        alert("works");
+        console.log("password check works");
+    } else alert("Enter numbers only");
 
 }
